cc5306c4364f25309ae4e475f14b05ec91b93484
Cherish0207 <cheshire_1207@foxmail.com>
2021-07-03 20:32:42 +0800

    4.WeakMap解决循环引用

--- 4.spread.深拷贝/3.deepClone.js
+++ 4.spread.深拷贝/3.deepClone.js
@@ -3,7 +3,8 @@
 
 // typeof instanceof toString constructor
 
-function deepClone(obj) {
+// hash:记录拷贝前和拷贝后的对应关系
+function deepClone(obj, hash = new WeakMap()) {
   if (obj == null) return obj; // null == undefined // true
   if (obj instanceof RegExp) return new RegExp(obj);
   if (obj instanceof Date) return new Date(obj);
@@ -11,19 +12,23 @@ function deepClone(obj) {
   if (typeof obj !== "object") return obj;
 
   // obj是对象类型( []、{})
+
+  if (hash.has(obj)) return hash.get(obj); // 返回上次拷贝的结果,不再递归
+
   // [].constructor; // ƒ Array
   // {}.constructor; // ƒ Object
   const copy = new obj.constructor();
+  hash.set(obj, copy); // 引用类型
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
-      copy[key] = deepClone(obj[key]);
+      copy[key] = deepClone(obj[key], hash);
     }
   }
   return copy;
 }
+// 拷贝过的对象,不需要再次拷贝
 var o = { a: "1", name: { n: "xxx" } };
-// o.b = {};
-// o.b.a = o.b;
-// const deepO = deepClone(o);// Maximum call stack size exceeded
-o.name.n = "aaa";
+o.b = {};
+o.b.a = o.b;
+const deepO = deepClone(o);
 console.log(deepO);
